177b59685fce22756b76ed3dac9702b471d42523
Dominik Hack <dominik.hack@squer.io>
2024-12-27 18:15:00 +0100

    Finish fiery path

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1226,8 +1226,8 @@
 #define FLAG_ITEM_ROUTE_101_METAGROSSITE                            0x494 // Unused Flag
 #define FLAG_ITEM_ROUTE_102_GALLADITE                               0x495 // Unused Flag
 #define FLAG_ITEM_ROUTE_112_PIDGEOTITE                              0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
-#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
+#define FLAG_ITEM_FIERY_PATH_CHARIZARDITE_X                         0x497 // Unused Flag
+#define FLAG_ITEM_FIERY_PATH_CHARIZARDITE_Y                         0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
